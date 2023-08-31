54d90f9691c94291ed1f54a756cd9ef3f226b469
vineetrj4196 <122870166+vineetrj4196@users.noreply.github.com>
2023-08-31 19:16:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+var num=document.querySelectorAll(".drum").length
+// console.log(num)
+
+for(var i=0;i<num;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        // alert("clicked")
+        var btn=this.innerHTML
+switch(btn){
+    
+        case 'w':var audio=new Audio("sounds/tom-1.mp3")
+        audio.play();
+        console.log("w")
+        break;
+        case 'a':var audio=new Audio("sounds/tom-2.mp3")
+        console.log("a")
+        break;
+        case 's':var audio=new Audio("sounds/tom-3.mp3")
+        audio.play();
+        console.log("s")
+        break;
+        case 'd':var audio=new Audio("sounds/tom-4.mp3")
+        console.log("d")
+        break;
+        case 'j':var audio=new Audio("sounds/crash.mp3")
+        console.log("j")
+        break;
+        case 'k':var audio=new Audio("sounds/kick-bass.mp3")
+        audio.play();
+        console.log("k")
+        break;
+        case 'l':var audio=new Audio("sounds/snare.mp3")
+        console.log("l")
+        break;
+       
+}
+    })
+}
+document.addEventListener("keypress",function(e){
+    var key=e.key 
+switch(key){
+    
+    case 'w':var audio=new Audio("sounds/tom-1.mp3")
+    audio.play();
+    console.log("w")
+    break;
+    case 'a':var audio=new Audio("sounds/tom-2.mp3")
+    console.log("a")
+    break;
+    case 's':var audio=new Audio("sounds/tom-3.mp3")
+    audio.play();
+    console.log("s")
+    break;
+    case 'd':var audio=new Audio("sounds/tom-4.mp3")
+    console.log("d")
+    break;
+    case 'j':var audio=new Audio("sounds/crash.mp3")
+    console.log("j")
+    break;
+    case 'k':var audio=new Audio("sounds/kick-bass.mp3")
+    audio.play();
+    console.log("k")
+    break;
+    case 'l':var audio=new Audio("sounds/snare.mp3")
+    console.log("l")
+    break;
+    default:console.log(e.key)
+}
+       
+    })
