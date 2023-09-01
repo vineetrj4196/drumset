5ed6550023e9c02e6b024dec71fcee0f8365d0c5
vineetrj4196 <122870166+vineetrj4196@users.noreply.github.com>
2023-09-01 12:32:54 +0530

    Update code.js

--- code.js
+++ code.js
@@ -7,31 +7,31 @@ for(var i=0;i<num;i++){
         var btn=this.innerHTML
 switch(btn){
     
-        case 'w':var audio=new Audio("sounds/tom-1.mp3")
+        case 'w':var audio=new Audio("tom-1.mp3")
         audio.play();
         console.log("w")
         break;
-        case 'a':var audio=new Audio("sounds/tom-2.mp3")
+        case 'a':var audio=new Audio("tom-2.mp3")
         audio.play();
         console.log("a")
         break;
-        case 's':var audio=new Audio("sounds/tom-3.mp3")
+        case 's':var audio=new Audio("tom-3.mp3")
         audio.play();
         console.log("s")
         break;
-        case 'd':var audio=new Audio("sounds/tom-4.mp3")
+        case 'd':var audio=new Audio("tom-4.mp3")
         audio.play();
         console.log("d")
         break;
-        case 'j':var audio=new Audio("sounds/crash.mp3")
+        case 'j':var audio=new Audio("crash.mp3")
         audio.play();
         console.log("j")
         break;
-        case 'k':var audio=new Audio("sounds/kick-bass.mp3")
+        case 'k':var audio=new Audio("kick-bass.mp3")
         audio.play();
         console.log("k")
         break;
-        case 'l':var audio=new Audio("sounds/snare.mp3")
+        case 'l':var audio=new Audio("snare.mp3")
         console.log("l")
         audio.play();
         break;
